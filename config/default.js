d2f73a799f8c4e2281ababae70ab3daff7690748
Jannis R <mail@jannisr.de>
2016-08-18 22:06:25 +0200

    HTTP auth :lock:

--- config/default.js
+++ config/default.js
@@ -4,5 +4,6 @@ module.exports = {
 	dnsPort: 53,
 	httpPort: 8053,
 	domain: 'example.org',
-	ttl: 300
+	ttl: 300,
+	key: '<your secret key here>'
 }
--- http.js
+++ http.js
@@ -1,9 +1,12 @@
 'use strict'
 
+const auth = require('http-auth')
 const http = require('http')
 const cfg = require('config')
 
-const server = http.createServer((req, res) => {
+const protect = auth.basic({}, (user, key, cb) => cb(key === cfg.key))
+
+const server = http.createServer(protect, (req, res) => {
 	res.end('foo')
 })
 
