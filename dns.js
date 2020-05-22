caf98846aba2cac6b5b4580fa7073b1b1424cf2a
Jannis R <mail@jannisr.de>
2020-05-23 01:57:20 +0200

    DNS server: respond with SOA record :boom:

--- dns.js
+++ dns.js
@@ -1,8 +1,29 @@
 'use strict'
 
 const dnsSocket = require('dns-socket')
-const {domain, ttl, hostname} = require('./lib/config')
+const {
+	domain, ttl,
+	hostname,
+	adminEmailDomain,
+	soaTtl,
+} = require('./lib/config')
 const logger = require('./lib/logger')
+
+const SOA = {
+	name: domain,
+	ttl,
+	class: 'IN',
+	type: 'SOA',
+	data: {
+		mname: hostname,
+		rname: adminEmailDomain,
+		serial: Date.now() / 1000 | 0,
+		refresh: soaTtl,
+		retry: Math.ceil(soaTtl / 2),
+		expire: 1209600, // 14 days
+		minimum: soaTtl,
+	},
+}
 
 const createServer = (getA, getAAAA) => {
 	const resolvers = Object.create(null)
@@ -15,22 +36,21 @@ const createServer = (getA, getAAAA) => {
 		.filter(q => q.class === 'IN')
 		// todo: support NS queries?
 		// todo: support PTR queries?
-		.filter(q => !!resolvers[q.type])
+		.filter(q => q.type === 'SOA' || !!resolvers[q.type])
 		.filter(q => q.name === domain)
 
 		const res = {
 			flags: dnsSocket.AUTHORITATIVE_ANSWER,
 			answers: [],
-			authorities: [{
-				type: 'NS',
-				class: 'IN',
-				name: domain,
-				ttl,
-				data: hostname
-			}]
+			authorities: [SOA],
 		}
 
 		for (const q of questions) {
+			if (q.type === 'SOA') {
+				res.answers.push(SOA)
+				continue
+			}
+
 			const address = resolvers[q.type]()
 			if (address === null) continue
 
